c1fa4c6c3cf6ba469a4edaeb8a2e604d0fdc2b50
DenisDov <dovzdenis@gmail.com>
2018-01-03 17:14:29 +0200

    first commit

---
+++ app/config/routes.js
@@ -0,0 +1,12 @@
+import { StackNavigator } from 'react-navigation';
+
+import Home from './../screens/Home';
+
+export default StackNavigator({
+  HomeScreen: {
+    screen: Home,
+    navigationOptions: {
+      headerTitle: 'Crypto Currencies',
+    },
+  },
+});
